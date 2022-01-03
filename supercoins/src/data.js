d9cfb6d11763af2694c9648071782a5e75bd7144
vankadavathashoknaik <90868746+vankadavathashoknaik@users.noreply.github.com>
2022-01-04 02:30:24 +0800

    changede v

--- supercoins/src/data.js
+++ supercoins/src/data.js
@@ -1,28 +1,28 @@
 export const productsData = [
   {
-    productname: 'mavic',
+    productname: 'GLIDER PLANES',
     description:
       'DJI Mavic 3 is a powerful flagship camera drone equipped with a 4/3 CMOS Hasselblad camera to facilitate professional-level imaging. It also offers omnidirectional obstacle sensing for a smooth flight experience, a max flight time of 46 minutes, and a max transmission range of 15 km. Every upgrade of DJI Mavic 3 sets a new standard for aerial photography',
     image: '../assets/carasoel1.png',
   },
   {
-    productname: 'mavic',
+    productname: 'TELONS',
     description:
       'DJI Mavic 3 is a powerful flagship camera drone equipped with a 4/3 CMOS Hasselblad camera to facilitate professional-level imaging. It also offers omnidirectional obstacle sensing for a smooth flight experience, a max flight time of 46 minutes, and a max transmission range of 15 km. Every upgrade of DJI Mavic 3 sets a new standard for aerial photography',
     image: '../assets/carasoel2.png',
   },
   {
-    productname: 'mavic',
+    productname: 'MULTIROATORS',
     description:
       'DJI Mavic 3 is a powerful flagship camera drone equipped with a 4/3 CMOS Hasselblad camera to facilitate professional-level imaging. It also offers omnidirectional obstacle sensing for a smooth flight experience, a max flight time of 46 minutes, and a max transmission range of 15 km. Every upgrade of DJI Mavic 3 sets a new standard for aerial photography',
     image: '../assets/carasoel3.png',
   },
-  // {
-  //   productname: 'mavic',
-  //   description:
-  //     'DJI Mavic 3 is a powerful flagship camera drone equipped with a 4/3 CMOS Hasselblad camera to facilitate professional-level imaging. It also offers omnidirectional obstacle sensing for a smooth flight experience, a max flight time of 46 minutes, and a max transmission range of 15 km. Every upgrade of DJI Mavic 3 sets a new standard for aerial photography',
-  //   image: '',
-  // },
+  {
+    productname: 'ENGINE PLANES',
+    description:
+      'DJI Mavic 3 is a powerful flagship camera drone equipped with a 4/3 CMOS Hasselblad camera to facilitate professional-level imaging. It also offers omnidirectional obstacle sensing for a smooth flight experience, a max flight time of 46 minutes, and a max transmission range of 15 km. Every upgrade of DJI Mavic 3 sets a new standard for aerial photography',
+    image: '',
+  },
   // {
   //   productname: 'mavic',
   //   description:
--- supercoins/src/products/Products.jsx
+++ supercoins/src/products/Products.jsx
@@ -1,25 +1,46 @@
 import React from 'react';
 import { productsData } from '../data';
 import './productStyles.css';
-import drone2 from '../assets/drone.png';
-import hexa from '../assets/hexa.png';
-import img1 from '../assets/carasoel1.png';
-import img2 from '../assets/carasoel2.png';
-import img3 from '../assets/carasoel3.png';
+import drone2 from '../assets/r.png';
+import hexa from '../assets/multi.png';
+import img1 from '../assets/rc.png';
+import img2 from '../assets/tel.png';
+import img3 from '../assets/multi.png';
 const Products = () => {
   const images = [img1, img2, img3, drone2];
   let c = 0;
   return (
+    <><section class="drones">
+      <h1>WELCOME TO AVIAVIN</h1>
+      <p>WE PROVIDE</p>
+      <div class="row">
+        <div class="columbs">
+          <div class="D">
+          <h3><a href="">SURVILLENCE</a></h3>
+        </div>
+        </div>
+      
+      <div class="columbs">
+        <div class="b">
+        <h3><a href="">WORKSHOP</a></h3>
+      </div></div>
+      <div class="columbs">
+        <div class="c">
+        <h3><a href="">SERVICE</a></h3>
+      </div>
+      </div>
+      </div>
+    </section>
     <div className="allProducts">
-      {productsData.map((product, c) =>(
-        // console.log(product);
-        <article className="productCard">
-          <h1 id="name">{product.productname}</h1>
-          <img id="image" src={images[c]} />
-          <p id="desc">{product.description}</p>
-        </article>
-      ))}
-    </div>
+        {productsData.map((product, c) => (
+          // console.log(product);
+          <article className="productCard">
+            <h1 id="name">{product.productname}</h1>
+            <img id="image" src={images[c]} />
+            <p id="desc">{product.description}</p>
+          </article>
+        ))}
+      </div></>
   );
 };
 
