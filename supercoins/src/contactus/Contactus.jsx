3d0ec1c380912ca56b21f240990ba72188735a2b
SAT <53694374+codewithSAT@users.noreply.github.com>
2022-01-03 23:27:15 +0530

    latest changes

--- supercoins/src/contactus/Contactus.jsx
+++ supercoins/src/contactus/Contactus.jsx
@@ -26,27 +26,53 @@ const Contactus = () => {
       //   setPerson({ firstname: '', lastname: '', email: '' });
       console.log(data);
       setshowform(false);
+      // setTimeout(() => (window.location.href = '/home'), 5000);
+    } else {
+      document.getElementById('firstname') == '' &&
+        document.getElementById('firstname').focus();
+      document.getElementById('email') == '' &&
+        document.getElementById('email').focus();
+      document.getElementById('lastname') == '' &&
+        document.getElementById('lastname').focus();
+      document.getElementById('phonenumber') == '' &&
+        document.getElementById('phonenumber').focus();
+      document.getElementById('description') == '' &&
+        document.getElementById('description').focus();
+      // document.getElementById('firstname').focus();
     }
   };
 
-  //   const handleDelete = (did) => {
-  //     const newList = data.filter((person) => {
-  //       if (person.id !== did) return true;
-  //     });
-  //     setCount(count - 1);
-  //     setdata(newList);
-  //   };
+  /*function sendEmail() {
+      Email.send({
+        Host: "smtp.gmail.com",
+        Username: "sender@email_address.com",
+        Password: "Enter your password",
+        To: 'receiver@email_address.com',
+        From: "sender@email_address.com",
+        Subject: "Sending Email using javascript",
+        Body: "Well that was easy!!",
+        Attachments: [
+          {
+            name: "File_Name_with_Extension",
+            path: "Full Path of the file"
+          }]
+      })
+        .then(function (message) {
+          alert("Mail has been sent successfully")
+        });
+    }*/
   return (
     <>
       {showform ? (
-        <div className="formLayout">
+        <form className="formLayout">
           <h2>
             We value your Interest. Please fill in the below details and submit
             your request.
           </h2>
           <div className="element">
-            <label htmlFor="firstname">First Name</label>
+            <label htmlFor="firstname">First Name </label>
             <input
+              required
               type="text"
               id="firstname"
               name="firstname"
@@ -57,8 +83,9 @@ const Contactus = () => {
           </div>
 
           <div className="element">
-            <label htmlFor="lastname">Last Name</label>
+            <label htmlFor="lastname">Last Name </label>
             <input
+              required
               type="text"
               id="lastname"
               name="lastname"
@@ -71,6 +98,7 @@ const Contactus = () => {
             <label htmlFor="email">Email </label>
 
             <input
+              required
               type="text"
               id="email"
               name="email"
@@ -83,6 +111,7 @@ const Contactus = () => {
             <label htmlFor="phonenumber">Contact number </label>
 
             <input
+              required
               type="text"
               id="phonenumber"
               name="phonenumber"
@@ -95,6 +124,7 @@ const Contactus = () => {
             <label htmlFor="description">Looking for </label>
 
             <textarea
+              required
               type="textArea"
               id="description"
               name="description"
@@ -108,7 +138,7 @@ const Contactus = () => {
               Submit Request
             </button>
           </div>
-        </div>
+        </form>
       ) : (
         <div className="formLayout">
           <h2>
@@ -116,29 +146,9 @@ const Contactus = () => {
             24 hours.
           </h2>
           <h3>Thank you for your Interest...!!</h3>
+          {/* <h3>Redirecting to home screen.</h3> */}
         </div>
       )}
-      {/* {data.map((person) => {
-          const { id, firstname, lastname, email } = person;
-        return (
-            <>
-            <h4 key={id}>
-              {firstname} - {lastname} - {email}
-              <button onClick={() => handleDelete(id)}>x</button>
-            </h4>
-          </>
-        );
-    })}
-      {count !== 0 && (
-          <button
-          onClick={() => {
-              setCount(0);
-            setdata([]);
-        }}
-        >
-          Remove all {count} users
-        </button>
-      )} */}
     </>
   );
 };
