8883dfe16af15b6089aefc7a221da3ac99694454
SAT <53694374+codewithSAT@users.noreply.github.com>
2022-01-14 00:11:37 +0530

    removal of unnecessary photos

--- aviavin/src/services/Services.jsx
+++ aviavin/src/services/Services.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { servicesData } from '../data';
-import img from '../assets/droneShot.jpg';
+import img1 from '../assets/cyberdrone.jpg';
+import img2 from '../assets/service1.jpg';
+import img3 from '../assets/droneassembly.jpg';
+import img4 from '../assets/droneCam.jpg';
+
 import './serviceStyles.css';
 
 const Services = () => {
+  const images = [img1, img2, img3, img4];
+  let count = 0;
+
   console.log(servicesData);
   return (
     <div className="services">
       {servicesData.map((service) => (
         <div className="service" key={service.id}>
-          <img src={img} alt="Smiley face" className="serviceImage"></img>
+          <img
+            src={images[count]}
+            alt="Smiley face"
+            className="serviceImage"
+          ></img>
+
           <div className="serviceContent">
             <h2>{service.servicename}</h2>
             <p>{service.description}</p>
